702d3729f7a6ff5072fd207aeb2df7cb6cd17389
wangshengyu <wangshengyu1987@foxmail.com>
2019-05-24 08:20:37 +0800

    generate rosdeb with version

--- py_tool_box/generate_ros_deb.py
+++ py_tool_box/generate_ros_deb.py
@@ -1,5 +1,24 @@
 import subprocess
+import re
+import os
+import shutil
+from py_tool_box import common
+from py_tool_box import generate_version
 def run():
-    subprocess.run(["rm", "-rf", "debian"])
+    if os.path.exists("debian") and os.path.isdir("debian"):
+        shutil.rmtree("debian")
+    if os.path.exists("build") and os.path.isdir("build"):
+        shutil.rmtree("build")
     subprocess.run(["bloom-generate", "rosdebian", "--ros-distro", "melodic"])
-    subprocess.run(["fakeroot", "debian/rules", "binary"])
+    result = common.get_cmd_output("fakeroot debian/rules binary")
+    result_lines = result.split("\n")
+    last_line = result_lines[-1]
+    m = re.search(".*building package '(.*)' in '(.*)'.", last_line)
+    package_name = m.group(1)
+    file_path = m.group(2)
+    version = generate_version.generate_full_version()
+    if version == None:
+        version = "1.0"
+    os.mkdir("build")
+    shutil.move(file_path, "build/" + package_name + "_" + version + ".deb")
+    
--- py_tool_box/generate_version.py
+++ py_tool_box/generate_version.py
@@ -2,39 +2,44 @@ import subprocess
 import re
 from py_tool_box import common
 
-def extract_version(raw_version):
-    m = re.search('VERSION_([0-9]*)_([0-9]*)-([0-9]*)-(.*)', raw_version)
+def extract_version(version_tag):
+    m = re.search('VERSION_([0-9]*)_([0-9]*)(.*)', version_tag)
     if m:
-        return m.group(1) + "." + m.group(2), m.group(3)
+        return m.group(1) + "." + m.group(2)
     else:
         print ("Generate version fail. Can't find a tag like VERSION_X_X")
         return None
 
 def get_version_by_tag():
     try:
-        raw_version = common.get_cmd_output("git describe --tags --match VERSION_[0-9]*_[0-9]*")
+        version_tag = common.get_cmd_output("git describe --tags --match VERSION_[0-9]*_[0-9]*")
     except Exception:
-        return "", None
-    version, subversion = extract_version(raw_version)
+        return None
+    version = extract_version(version_tag)
     if version == None:
-        return "", None
+        return None, None
+    tag_commit = common.get_cmd_output("git rev-list -n 1 " + version_tag)
+    subversion = common.get_cmd_output("git rev-list --count HEAD.." + tag_commit)
     return version, subversion
 
 def is_git_repo():
     try:
-        get_output("git branch")
+        common.get_cmd_output("git branch")
         return True
     except Exception:
         return False
 
-def run():
+def generate_full_version():
     if not is_git_repo():
-        return
+        return ""
     version, subversion = get_version_by_tag()
-    if version == "":
+    if version == None:
         version = "0"
         subversion = common.get_cmd_output("git rev-list --count HEAD")
     commit = common.get_cmd_output("git log --format=%h -n 1")
 
-    print(version + "." + subversion + "." + commit)
+    return version + "." + subversion + "." + commit
+
+def run():
+    print (generate_full_version())
 
